420cca0400daf8c7dd1bac7c41ddee44e6b6c344
LordRatte <joshuastubbs6@gmail.com>
2019-11-06 15:17:56 +0200

    Update duolinguo.js

--- duolinguo.js
+++ duolinguo.js
@@ -1,6 +1,14 @@
-Game.customCpsMult.push(function(){
-console.log("tick");
+if (type Game.lordratte === 'undefined') Game.lordratte = {};
+if (type Game.lordratte.duolinguo_streak === 'undefined') Game.lordratte.duolinguo_streak = 0;
 
-    
-return 1;
+Game.customCpsMult.push(function(){
+    var user = 'lordratte';
+    var url = 'https://www.duolingo.com/users/{}'.replace('{}', user);
+    ajax(url, function(r, a){
+        console.log(r);
+        console.log(a);
+    });
+
+
+    return 1;
 });
